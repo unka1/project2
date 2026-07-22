79283f76f12cf318340e4bc682a23f0bf11ccacd
unka1 <122786706+unka1@users.noreply.github.com>
2026-07-23 00:43:10 +0530

    Add files via upload

---
+++ frontend/src/components/About.jsx
@@ -0,0 +1,13 @@
+function About(){
+    return (
+      <section id="about">
+        <h1 className="lfh1">About Us</h1>
+        
+        <footer>
+            
+        </footer>
+      </section>
+      
+    );
+}
+export default About;
